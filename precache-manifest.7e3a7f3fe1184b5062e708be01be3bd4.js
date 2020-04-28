4244e12b272318ffe4b61c45640e3978d7c17965
Shreets <shreeti.019@gmai;.com>
2020-04-28 19:37:07 +0545

    Updates

---
+++ precache-manifest.7e3a7f3fe1184b5062e708be01be3bd4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c1585533db445affef1c12b879efee",
+    "url": "/vivid-covid/index.html"
+  },
+  {
+    "revision": "aebeb150a87215ec4456",
+    "url": "/vivid-covid/static/css/main.5f093186.chunk.css"
+  },
+  {
+    "revision": "c412c63f45b49bfe8d2e",
+    "url": "/vivid-covid/static/js/2.f4ba299e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vivid-covid/static/js/2.f4ba299e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebeb150a87215ec4456",
+    "url": "/vivid-covid/static/js/main.f359043e.chunk.js"
+  },
+  {
+    "revision": "4f106f1d827b408c7bdd",
+    "url": "/vivid-covid/static/js/runtime-main.88d1b780.js"
+  },
+  {
+    "revision": "f12357ebc8cc59418131c6b9b1d5b392",
+    "url": "/vivid-covid/static/media/chart.f12357eb.png"
+  },
+  {
+    "revision": "7148e7f5866572a6ee35b6175ad35150",
+    "url": "/vivid-covid/static/media/hover.7148e7f5.png"
+  },
+  {
+    "revision": "8fe3dbf23f7ecb468380c943cdce0400",
+    "url": "/vivid-covid/static/media/locate.8fe3dbf2.png"
+  },
+  {
+    "revision": "90f98b6e1ccd3491ca5599a8ccb5d8c6",
+    "url": "/vivid-covid/static/media/logo.90f98b6e.png"
+  }
+]);
